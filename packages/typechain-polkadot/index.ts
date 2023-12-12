8604b9d1ed4ec08b273083c1c3cfec3763dca211
Danylo Semirazov <semirazov94@gmail.com>
2023-12-12 19:38:18 +0000

    update vers

--- packages/typechain-polkadot/index.ts
+++ packages/typechain-polkadot/index.ts
@@ -33,8 +33,8 @@
  *  @example
  *  # Usage from CLI
  *  ```bash
- *     $ npm i @727-ventures/typechain-polkadot
- *     $ npx @727-ventures/typechain-polkadot --in path/to/abis --out path/to/output/folder
+ *     $ npm i @archisinal/typechain-polkadot
+ *     $ npx @archisinal/typechain-polkadot --in path/to/abis --out path/to/output/folder
  *  ```
  *
  *  @packageDocumentation
